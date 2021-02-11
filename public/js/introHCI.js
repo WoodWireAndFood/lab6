ed84fb47207ab1777234011a4cd27d889c696df6
Mark <gitcommits@mkwsystems.com>
2021-02-11 10:24:01 -0800

    Clicking expands project information

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+	$('.project a').click(addProjectDetails); //click handler
 
 	// $('#colorBtn').click(randomizeColors);
 }
@@ -25,6 +25,20 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	var projectURL = "/project/" + idNumber;
+	$.get(projectURL, projectHandler);
+	console.log("Getting " + projectURL);
+	var fullID = "project" + idNumber;
+	console.log(fullID);
+	console.log($("#" + fullID + " .details").html());
 	console.log("User clicked on project " + idNumber);
+}
+
+
+function projectHandler(result) {
+	var fullID = "project" + result.id;
+	var htmlToAdd = '<p>' + result.date + '<p> <img class="detailsImage" src="' + result.image + '">' + result.summary;
+	$("#" + fullID + " .details").html(htmlToAdd);
+	console.log(result);
+
 }
